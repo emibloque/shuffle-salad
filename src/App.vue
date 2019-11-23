e05beec97ba0d26291c11a4094b155a593ca43c3
Luis Emilio Velasco Sánchez <emibloque@gmail.com>
2019-11-23 03:47:43 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <main>
+      <h1>Crea tu ensalada:</h1>
+      <ul>
+        <li><strong>Base:</strong> {{ base }}</li>
+        <li><strong>Ingredientes:</strong> {{ ingredients }}</li>
+        <li><strong>Salsa:</strong> {{ sauce }}</li>
+      </ul>
+      <p-button>Crear</p-button>
+    </main>
+  </div>
+</template>
+
+<script>
+import PButton from './components/PButton.vue'
+
+export default {
+  data: () => ({
+    base: '',
+    ingredients: '',
+    sauce: ''
+  }),
+
+  components: {
+    PButton
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#app > main {
+  min-width: 300px;
+}
+</style>
---
+++ src/components/PButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PButton'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/variables';
+
+.btn {
+  background-color: $color-primary;
+  color: $color-text-primary;
+  font-size: $font-size;
+  font-family: $font-family;
+  padding: $spacing-small $spacing-medium;
+  border-radius: $border-radious;
+
+
+  width: 100%;
+  text-transform: uppercase;
+  border: none;
+  cursor: pointer;
+  transition: background-color .15s, transform .15s;
+
+  &:hover {
+    background-color: $color-primary-hover;
+    transform: translateY(-1px);
+    box-shadow: 0 3px 2px rgba(0,0,0,0.2);
+  }
+
+  &:active {
+    background-color: $color-primary-active;
+  }
+}
+</style>
